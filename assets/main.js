d9d000551569aac8da90666d0c67403ee4cadb53
Kauã Silva <161160708+almeid4kaua@users.noreply.github.com>
2024-08-26 00:25:09 -0300

    Definindo layout

---
+++ assets/main.js
@@ -0,0 +1,85 @@
+
+
+window.revelar = ScrollReveal({reset:true})
+
+            // SECTION ABOUT
+
+                revelar.reveal('.img-topo',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 200,
+                        origin: 'left'
+                    })
+
+                revelar.reveal('.txt-about',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 400,
+                        origin: 'right'
+                    })
+
+                revelar.reveal('.txt-about1',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 500,
+                        origin: 'right'
+                    })
+
+                revelar.reveal('.txt-about2',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 600,
+                        origin: 'right'
+                    })
+
+                revelar.reveal('.txt-about3',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 700,
+                        origin: 'right'
+                    })
+
+                revelar.reveal('.icon1',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 100,
+                        origin: 'left'
+                    })
+
+                revelar.reveal('.icon2',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 200,
+                        origin: 'left'
+                    })
+
+                revelar.reveal('.icon3',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 300,
+                        origin: 'left'
+                    })
+
+                revelar.reveal('.icon4',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 400,
+                        origin: 'left'
+                    })
+
+                revelar.reveal('.icon5',
+                    {
+                        duration: 2000,
+                        distance: '90px',
+                        delay: 500,
+                        origin: 'left'
+                    })
